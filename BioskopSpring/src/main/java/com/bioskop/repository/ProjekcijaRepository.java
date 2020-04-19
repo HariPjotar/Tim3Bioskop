853b1a407a8e4b4e21a23e0d8e095137ee41a2f8
Aleksandra Romanic <aleksandra.romanic@gmail.com>
2020-04-19 16:08:50 +0200

    Upit za vracanje projekcija po datumu @T2472

--- BioskopSpring/src/main/java/com/bioskop/repository/ProjekcijaRepository.java
+++ BioskopSpring/src/main/java/com/bioskop/repository/ProjekcijaRepository.java
@@ -1,9 +1,17 @@
 package com.bioskop.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import model.Projekcija;
 
 public interface ProjekcijaRepository extends JpaRepository<Projekcija, Integer> {
+	
+	@Query("SELECT p FROM Projekcija p WHERE p.datum BETWEEN :startDate AND :endDate")
+	public List<Projekcija> vratiNedeljniRepertoar(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
 
 }
